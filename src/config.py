57aecd8e7919f8690793c4e42839ee4f7de04a51
Shwayke <pinskyshay@gmail.com>
2026-01-13 21:35:07 +0200

    fix environment variable name

--- src/config.py
+++ src/config.py
@@ -6,14 +6,14 @@ load_dotenv()
 
 def get_api_url():
     """Get API URL from environment"""
-    url = os.environ.get('API_URL')
+    url = os.environ.get('IMS_API_URL')
     if not url:
-        raise ValueError("API_URL environment variable is not set!")
+        raise ValueError("IMS_API_URL environment variable is not set!")
     return url
 
 def get_api_key():
     """Get API key from environment"""
-    key = os.environ.get('API_KEY')
+    key = os.environ.get('IMS_API_KEY')
     if not key:
-        raise ValueError("API_KEY environment variable is not set!")
+        raise ValueError("IMS_API_KEY environment variable is not set!")
     return key
